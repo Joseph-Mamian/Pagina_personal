d649ccb199187c4d00ed9e9ff815b70c73184065
Joseph-Mamian <jsmamianb@ufpso.edu.co>
2023-11-28 13:17:30 -0500

    uploading-personal-page

--- js/functions.js
+++ js/functions.js
@@ -2,17 +2,8 @@ function confirmar(){
     Swal.fire({
         icon: 'success',
         ShowConfirmButton: false,
-        text: "tabla guardada",
+        text: "TABLA GUARDADA",
         background: 'white',
         position:'center' 
     });
-}
-function eliminar_cambios(){
-    Swal.fire({
-        icon: 'success',
-        ShowConfirmButton: true,
-        text: "tabla eliminada",
-        background: 'white',
-    });
-    document.getElementById("eliminar").reset();
 }
